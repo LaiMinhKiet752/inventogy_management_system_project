020e21b75e782d4253b5209269db8e47a7fb928b
Minh Kiet <kietminh070502@gmail.com>
2023-04-12 20:43:36 +0700

    Add Full Invoice And Save To Database

--- resources/views/backend/invoice/invoice_all.blade.php
+++ resources/views/backend/invoice/invoice_all.blade.php
@@ -33,7 +33,7 @@
                                         <th>Invoice No</th>
                                         <th>Date</th>
                                         <th>Description</th>
-                                        <th>Action</th>
+                                        <th>Amount</th>
 
                                 </thead>
 
@@ -43,17 +43,12 @@
                                     @foreach ($allData as $key => $item)
                                         <tr>
                                             <td> {{ $key + 1 }} </td>
-                                            <td> </td>
+                                            <td> {{ $item['payment']['customer']['name'] }} </td>
                                             <td> {{ $item->invoice_no }} </td>
                                             <td> {{ date('d-m-Y', strtotime($item->date)) }} </td>
                                             <td> {{ $item->description }} </td>
                                             <td>
-                                                @if ($item->status == '0')
-                                                    <a href="{{ route('purchase.delete', $item->id) }}"
-                                                        class="btn btn-danger sm" title="Delete Data" id="delete"> <i
-                                                            class="fas fa-trash-alt"></i>
-                                                    </a>
-                                                @endif
+                                               $ {{ $item['payment']['total_amount'] }}
                                             </td>
 
                                         </tr>
