0db02cebc729c13fb8e06cbd774f59f42222a476
Minh Kiet <kietminh070502@gmail.com>
2023-04-20 16:28:12 +0700

    Add Customer Wise-Credit-Paid

--- app/Http/Controllers/Pos/CustomerController.php
+++ app/Http/Controllers/Pos/CustomerController.php
@@ -159,15 +159,34 @@ class CustomerController extends Controller
     {
         $payment = Payment::where('invoice_id', $invoice_id)->first();
         return view('backend.pdf.invoice_details_pdf', compact('payment'));
+    } //End Method
+
+    public function PaidCustomer()
+    {
+        $allData = Payment::where('paid_status', '!=', 'full_due')->get();
+        return view('backend.customer.customer_paid', compact('allData'));
+    } //End Method
+
+    public function PaidCustomerPrintPdf()
+    {
+        $allData = Payment::where('paid_status', '!=', 'full_due')->get();
+        return view('backend.pdf.customer_paid_pdf', compact('allData'));
+    } //End Method
+
+    public function CustomerWiseReport()
+    {
+        $customers = Customer::all();
+        return view('backend.customer.customer_wise_report', compact('customers'));
+    } //End Method
+
+    public function CustomerWiseCreditReport(Request $request)
+    {
+        $allData = Payment::where('customer_id', $request->customer_id)->whereIn('paid_status', ['full_due', 'partial_paid'])->get();
+        return view('backend.pdf.customer_wise_credit_pdf', compact('allData'));
     }//End Method
 
-    public function PaidCustomer(){
-        $allData = Payment::where('paid_status','!=','full_due')->get();
-        return view('backend.customer.customer_paid',compact('allData'));
-    }//End Method
-
-    public function PaidCustomerPrintPdf(){
-        $allData = Payment::where('paid_status','!=','full_due')->get();
-        return view('backend.pdf.customer_paid_pdf',compact('allData'));
+    public function CustomerWisePaidReport(Request $request){
+        $allData = Payment::where('customer_id', $request->customer_id)->where('paid_status', '!=', 'full_due')->get();
+        return view('backend.pdf.customer_wise_paid_pdf', compact('allData'));
     }
 }
